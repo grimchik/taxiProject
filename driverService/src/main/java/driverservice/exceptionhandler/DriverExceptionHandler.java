3e49e473b9ef19c55aae0779fe10a3573f8f8625
grimch1k <sashavoitko2005@gmail.com>
2025-02-20 19:28:17 +0300

    Added Global Handler

---
+++ driverService/src/main/java/driverservice/exceptionhandler/DriverExceptionHandler.java
@@ -0,0 +1,57 @@
+package driverservice.exceptionhandler;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+@Hidden
+public class DriverExceptionHandler {
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<?> handleEntityExsistsException(EntityExistsException ex)
+    {
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return new ResponseEntity<>("Invalid values provided. Please check the input data.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        StringBuilder errorMessage = new StringBuilder("Validation errors: ");
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errorMessage.append("Field '")
+                        .append(error.getField())
+                        .append("' - ")
+                        .append(error.getDefaultMessage())
+                        .append(". ")
+        );
+        return new  ResponseEntity<>(errorMessage.toString().trim(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex)
+    {
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handleIllegalStateException(IllegalStateException ex)
+    {
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex)
+    {
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
