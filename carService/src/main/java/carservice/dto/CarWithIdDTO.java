7d969e251f97ae5f98ad35a8444f9398d51ae834
grimchik <112716265+grimchik@users.noreply.github.com>
2025-02-24 13:38:45 +0300

    Added Dto's. repository

---
+++ carService/src/main/java/carservice/dto/CarWithIdDTO.java
@@ -0,0 +1,19 @@
+package carservice.dto;
+
+import carservice.enums.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarWithIdDTO {
+    private Long id;
+    private String brand;
+    private String model;
+    private String description;
+    private String color;
+    private Category category;
+    private String number;
+}
