99565b6360b7ea6e41130438ecebc7ee07434809
grimchik <112716265+grimchik@users.noreply.github.com>
2025-02-24 22:33:04 +0300

    Added Dto's Repository Entity

---
+++ clientService/src/main/java/clientservice/dto/PhoneDTO.java
@@ -0,0 +1,15 @@
+package clientservice.dto;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PhoneDTO {
+    @Pattern(
+            regexp = "^\\+375\\d{9}$",
+            message = "phone number must be in the format +375DDDDDDDDD"
+    )
+    private String phone;
+}
---
+++ clientService/src/main/java/clientservice/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package clientservice.repository;
+
+import clientservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByPhone(String phone);
+    List<User> findAllByIsDeletedFalse();
+    @Modifying
+    @Query("UPDATE User u SET u.isDeleted = true WHERE u.username = :username")
+    void softDeleteByUsername(@Param("username") String username);
+}
