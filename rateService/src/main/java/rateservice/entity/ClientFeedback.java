d92ceb48f67f8c8d6ded67fa07b260ca468b3f7f
grimch1k <sashavoitko2005@gmail.com>
2025-02-26 18:43:23 +0300

    Added Entity Repository

---
+++ rateService/src/main/java/rateservice/entity/ClientFeedback.java
@@ -0,0 +1,33 @@
+package rateservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Table(name="client_feedbacks")
+public class ClientFeedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "rate",nullable = false)
+    private Double rate;
+
+    @Column(nullable = true)
+    private String comment;
+
+    @Column(nullable = true)
+    private Boolean cleanInterior;
+
+    @Column(nullable = true)
+    private Boolean safeDriving;
+
+    @Column(nullable = false)
+    private Boolean niceMusic;
+}
---
+++ rateService/src/main/java/rateservice/entity/DriverFeedback.java
@@ -0,0 +1,33 @@
+package rateservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+@Table(name="driver_feedbacks")
+public class DriverFeedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "comment",nullable = true)
+    private String comment;
+
+    @Column(name ="rate",nullable = false)
+    private Double rate;
+
+    @Column(nullable = true)
+    private Boolean politePassenger;
+
+    @Column(nullable = true)
+    private Boolean cleanPassenger;
+
+    @Column(nullable = true)
+    private Boolean punctuality;
+}
