7845578362e5bd5e8081513feae54a58952df736
grimch1k <sashavoitko2005@gmail.com>
2025-03-01 13:44:25 +0300

    Fixed urls

--- rateService/src/main/java/rateservice/configuration/SecurityConfig.java
+++ rateService/src/main/java/rateservice/configuration/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/driver-feedback/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/v1/driver-feedbacks/**","/api/v1/client-feedbacks/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
