057bf0fcae920b975c84c4f54e6964fe7c93dece
grimchik <112716265+grimchik@users.noreply.github.com>
2025-03-03 14:47:54 +0300

    Added eureka config

--- rateService/src/main/java/rateservice/RateServiceApplication.java
+++ rateService/src/main/java/rateservice/RateServiceApplication.java
@@ -3,7 +3,11 @@ package rateservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class RateServiceApplication {
     public static void main (String[] args)
     {
