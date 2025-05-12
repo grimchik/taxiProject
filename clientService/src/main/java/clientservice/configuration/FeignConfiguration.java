6d0b4093d55bf252857d6692b926b7a0d52419fa
grimchik <112716265+grimchik@users.noreply.github.com>
2025-05-12 20:55:23 +0300

    Added jwt to feign client

--- clientService/src/main/java/clientservice/configuration/FeignConfiguration.java
+++ clientService/src/main/java/clientservice/configuration/FeignConfiguration.java
@@ -1,10 +1,13 @@
 package clientservice.configuration;
 
 import clientservice.errordecoder.ClientErrorDecoder;
+import feign.RequestInterceptor;
 import feign.okhttp.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import feign.codec.ErrorDecoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 public class FeignConfiguration {
@@ -16,5 +19,15 @@ public class FeignConfiguration {
     @Bean
     public OkHttpClient client() {
         return new OkHttpClient();
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.getCredentials() instanceof String jwt) {
+                requestTemplate.header("Authorization", "Bearer " + jwt);
+            }
+        };
     }
 }
--- driverService/src/main/java/driverservice/configuration/FeignConfiguration.java
+++ driverService/src/main/java/driverservice/configuration/FeignConfiguration.java
@@ -1,10 +1,13 @@
 package driverservice.configuration;
 
 import driverservice.errordecoder.ClientErrorDecoder;
+import feign.RequestInterceptor;
 import feign.codec.ErrorDecoder;
 import feign.okhttp.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 public class FeignConfiguration {
@@ -16,5 +19,15 @@ public class FeignConfiguration {
     @Bean
     public OkHttpClient client() {
         return new OkHttpClient();
+    }
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.getCredentials() instanceof String jwt) {
+                requestTemplate.header("Authorization", "Bearer " + jwt);
+            }
+        };
     }
 }
